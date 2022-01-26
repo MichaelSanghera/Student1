ee1144126b70f81e9d7516cc608fdfaf1a0431a7
MichaelSanghera <98056575+MichaelSanghera@users.noreply.github.com>
2022-01-26 09:01:51 -0500

    added comments

--- src/student/Student2.java
+++ src/student/Student2.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package student;
 
 /**
- *
- * @author HP
+ *Jan 26,2022
+ * @author Michael Sanghera
  */
 public class Student2 {
     private String studentId;
